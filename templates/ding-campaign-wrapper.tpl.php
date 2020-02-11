67db22b6d257aea4c71a455413eef2501b74c3a2
Nicolae Grigoriu <ng@inlead.dk>
2020-02-11 23:48:46 +0200

    EASYOPAC-1224 - Consider campaing render based on type settings.

--- templates/ding-campaign-wrapper.tpl.php
+++ templates/ding-campaign-wrapper.tpl.php
@@ -2,13 +2,8 @@
 /**
  * @file
  * Default template for image campaigns.
- *
- * @var string|null $campaign_link
- * @var string|null $campaign_image
- * @var string $title
  */
-$image = "<img src='$campaign_image' alt='$title' />";
 ?>
 <div class="ding-campaign-wrapper">
-  <?php print l($image, $campaign_link, ['html' => TRUE, 'attributes' => ['title' => $title]]); ?>
+  <?php print drupal_render($content); ?>
 </div>
